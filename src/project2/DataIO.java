a165e4d2d292b6cb85912c918fd7160fec415608
BlastoiseBlue <emmet@stanevich.com>
2018-01-31 12:50:18 -0500

    Started migrating methods to the main class

--- src/project2/DataIO.java
+++ src/project2/DataIO.java
@@ -7,24 +7,52 @@ public class DataIO {
 	/*
 	 * readData reads text from a file and stores it in two arrays
 	 */
-	String[] myNames;
+	String[][] myNames;
 	double[][] stuScores;
+	char[] grades;
 	String inputFile = "project2IN.txt";
 
-	public void printData() {
-		for (int i = 0; i < myNames.length; i++) {
-			System.out.print(myNames[i] + ":");
-			for (int j = 0; j < stuScores.length; j++) {
-				System.out.print(stuScores[i][j]);
-				System.out.println();
-			}
-		}
+	char grade(double grade) {
+		if (grade >= 89.5)
+			return 'A';
+		else if (grade >= 82.5)
+			return 'B';
+		else if (grade >= 72.5)
+			return 'C';
+		else if (grade >= 62.5)
+			return 'D';
+		else
+			return 'F';
 	}
+	/*
+	 * public void printData() { for (int i = 0; i < myNames.length; i++) {
+	 * System.out.print(myNames[i] + ":\n"); for (int j = 0; j <
+	 * stuScores[i].length; j++) { System.out.print(stuScores[i][j] + "\t");
+	 * 
+	 * } System.out.println(); } }
+	 */
 
 	public DataIO(String fileIn) {
 		inputFile = fileIn;
 	}
 
+	public DataIO() {
+
+	}
+
+	/*
+	 * Reads the data in the specified file and stores is in local arrays
+	 * 
+	 * @fileIn String The name of the file
+	 */
+	public void readData(String fileIn) {
+		inputFile = fileIn;
+		readData();
+	}
+
+	/*
+	 * Reads the data in an already-specified file and stores it in local arrays
+	 */
 	public void readData() {
 		Scanner myIn = null; // Initializes fileIn to empty
 		String first, last;
@@ -35,27 +63,29 @@ public class DataIO {
 			lineCounter = new Scanner(new FileInputStream(inputFile));
 			int n = 0;
 			while (lineCounter.hasNextLine()) {
+				@SuppressWarnings("unused") // This placeholder is mainly used to force the counter to advance to the
+											// next line
 				String placeHolder = lineCounter.nextLine();
 				n++;
 			}
-			myNames = new String[n];
+			myNames = new String[n][2];
 			stuScores = new double[n][4];
+			grades = new char[n];
 			System.out.println("Opening input file: " + inputFile);
 			int i = 0;
-			//while (myIn.hasNextLine()) {
-				for(i=0;i<n;i++) {
-				//lineCounter = new Scanner(myIn.nextLine());
+			for (i = 0; i < n; i++) {
 				first = myIn.next();
 				last = myIn.next();
-				myNames[i] = (last + ", " + first);
+				myNames[i][0] = first;
+				myNames[i][1] = last;
 				int j = 0;
 				double total = 0;
 				for (j = 0; j < 3; j++) {
 					stuScores[i][j] = myIn.nextDouble();
 					total += stuScores[i][j]; // Keeps track of the total scores
 				}
-				stuScores[i][j] = total / (stuScores[i].length-1); // Stores the average score in the next slot
-				
+				stuScores[i][j] = total / (stuScores[i].length - 1); // Stores the average score in the next slot
+				grades[i] = grade(stuScores[i][j]);
 			}
 			System.out.println("Closing input file: " + inputFile);
 			myIn.close();
--- src/project2/StanevichHenningGradebook.java
+++ src/project2/StanevichHenningGradebook.java
@@ -4,11 +4,28 @@ package project2;
  * @authors Emmet Stanevich and Miranda Henning
  */
 public class StanevichHenningGradebook {
+	/*
+	 * Prints out the data from the stored memory
+	 */
+	static void printData(DataIO input) {
+		for (int i = 0; i < input.myNames.length; i++) {
+			System.out.print(input.myNames[i][1] + ", " + input.myNames[i][0] + ":\n");
+			for (int j = 0; j < input.stuScores[i].length; j++) {
+				System.out.print(input.stuScores[i][j] + "\t");
+			}
+			System.out.println("\n Grade: "+input.grades[i]);
+		}
+	}
 
+	/*
+	 * This will read an input file, reorder the names to last, first and print out
+	 * each name, the corresponding score, and the average score. After that, it
+	 * will output the results into project2OUT.txt
+	 */
 	public static void main(String[] args) {
 		DataIO students = new DataIO("C:\\Users\\emmet\\eclipse-workspace\\Semester2\\bin\\project2\\project2IN.txt");
 		students.readData();
-		students.printData();
+		printData(students);
 	}
 
 }
