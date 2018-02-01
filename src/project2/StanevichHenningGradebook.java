6625bfb14dcb1cbefe8d57144c3fc0221cff3fe6
BlastoiseBlue <emmet@stanevich.com>
2018-02-01 15:11:04 -0500

    Slight cleanup

--- src/project2/StanevichHenningGradebook.java
+++ src/project2/StanevichHenningGradebook.java
@@ -6,17 +6,18 @@ package project2;
 public class StanevichHenningGradebook {
 	/*
 	 * Prints out the data from the stored memory
+	 * 
+	 * @param input The DataIO object that has the 
 	 */
 	static void printClean(DataIO input) {
 		for (int i = 0; i < input.myNames.length; i++) {
-			System.out.print(input.myNames[i][1] + ", " + input.myNames[i][0] + ":\n");
+			System.out.println(input.myNames[i][1] + ", " + input.myNames[i][0] + ":");
 			for (int j = 0; j < input.stuScores[i].length; j++) {
 				System.out.print(input.stuScores[i][j] + "\t");
 			}
 			System.out.println("\n Grade: " + input.grades[i]);
 		}
 	}
-
 	/*
 	 * This will read an input file, reorder the names to last, first and print out
 	 * each name, the corresponding score, and the average score. After that, it
