89b8303de4fc78fb7848d17c93873d2b0e76b62b
BlastoiseBlue <emmet@stanevich.com>
2018-03-01 14:45:12 -0500

    The exceptions are working now

--- src/project4/Driver.java
+++ src/project4/Driver.java
@@ -13,6 +13,7 @@ import java.util.InputMismatchException;
 
 public class Driver {
 	static void ReturnRatio() {
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		int numerator = 0, denominator = 0;
 		double ratio;
@@ -20,24 +21,46 @@ public class Driver {
 		do {
 			error = false;
 			try {
-				
 				System.out.println("Please input the numerator:");
 				numerator = scan.nextInt();
-				if(numerator<0)throw new NegativeInputException();
-				
-			} catch (Exception e) {
-				//error = true;
+				if (numerator < 0)
+					throw new NegativeInputException(); // Throws an exception if the input is negative
+			} catch (InputMismatchException e) {
+				error = true;
+				scan.nextLine();
+				System.out.println("ERROR: INPUT MISMATCH");
+			} catch (NegativeInputException e) {
+				error = true;
 				System.out.println(e.getMessage());
+				scan.nextLine();
+			} catch (Exception e) {
+				error = true;
+				System.out.println(e.getMessage());
+				scan.nextLine();
 			}
 		} while (error);
 		do {
-			error=false;
+			error = false;
 			try {
 				error = false;
 				System.out.println("Please input the denominator:");
 				denominator = scan.nextInt();
-				if (denominator==0)throw new DivideByZeroException();
-				if (denominator<0)throw new NegativeInputException();
+				if (denominator == 0)
+					throw new DivideByZeroException(); // Throws an exception if the denominator is zero
+				if (denominator < 0)
+					throw new NegativeInputException();
+			} catch (InputMismatchException e) {
+				error = true;
+				scan.nextLine();
+				System.out.println(e.getMessage());
+			} catch (NegativeInputException e) {
+				error = true;
+				System.out.println(e.getMessage());
+				scan.nextLine();
+			} catch (DivideByZeroException e) {
+				error = true;
+				System.out.println(e.getMessage());
+				scan.nextLine();
 			} catch (Exception e) {
 				error = true;
 				System.out.println(e.getMessage());
